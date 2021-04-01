197f0c00ae9a6501f1fc6a1ef362e01043062a2a
Kritika <24kritikasharma24@gmail.com>
2021-04-01 21:51:27 +0530

    fetched data from api

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,12 @@
-import { allProductsUrl } from './utils.js';
+import { allProductsUrl } from './utils.js'
 
-const fetchProducts = async () => {};
+// fetching products from the api in the form of array- response
+const fetchProducts = async () => {
+  const response = await fetch(allProductsUrl).catch((err) => console.log(err))
+  if (response) {
+    return response.json()
+  }
+  return response
+}
 
-export default fetchProducts;
+export default fetchProducts
