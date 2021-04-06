9c3781551ec9b3c713867c86f8ea849a0caefcc7
Kritika <24kritikasharma24@gmail.com>
2021-04-06 07:54:42 +0530

    set up the store using api request and collect products plus their proprties

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ import display from './src/displayProducts.js'
 import { getElement } from './src/utils.js'
 const init = async () => {
   const products = await fetchProducts()
+  if (products) {
+    // add products to the store
+    setupStore(products)
+  }
 }
 window.addEventListener('DOMContentLoaded', init)
--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
-import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
-const setupStore = () => {};
-const findProduct = () => {};
-export { store, setupStore, findProduct };
+import { getStorageItem, setStorageItem } from './utils.js'
+let store = []
+const setupStore = (products) => {
+  store = products.map((product) => {
+    // destructuring the product -:
+    const {
+      id,
+      fields: { featured, price, name, company, colors, image: img },
+    } = product
+    const image = img[0].thumbnails.large.url
+    return { id, featured, price, name, company, colors, image }
+  })
+}
+const findProduct = () => {}
+export { store, setupStore, findProduct }
