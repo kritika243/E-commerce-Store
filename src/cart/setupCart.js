8f7a137e9f448db868c94132d19ff8e43fb50c24
Kritika <24kritikasharma24@gmail.com>
2021-04-20 15:27:13 +0530

    updating items in cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -27,6 +27,10 @@ export const addToCart = (id) => {
     addToCartDOM(product)
   } else {
     // update values
+    const amount = increaseAmount(id)
+    const items = [...cartItemsDOM.querySelectorAll('.cart-item-amount')]
+    const newAmount = items.find((value) => value.dataset.id === id)
+    newAmount.textContent = amount
   }
   // add one to the item count
   displayCartItemCount()
@@ -35,6 +39,18 @@ export const addToCart = (id) => {
   // set cart in local storage
   setStorageItem('cart', cart)
   openCart()
+}
+
+function increaseAmount(id) {
+  let newAmount
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1
+      cartItem = { ...cartItem, amount: newAmount }
+    }
+    return cartItem
+  })
+  return newAmount
 }
 
 function displayCartItemCount() {
