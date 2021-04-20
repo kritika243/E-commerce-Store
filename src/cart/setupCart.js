8581268b32dd43cd6cbe860e1bf249a6ab7c9722
Kritika <24kritikasharma24@gmail.com>
2021-04-20 12:35:36 +0530

    persisting cart through all pages

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,6 +10,16 @@ import { findProduct } from '../store.js'
 import addToCartDOM from './addToCartDOM.js'
 // set items
 
+const cartItemCountDOM = getElement('.cart-item-count')
+const cartItemsDOM = getElement('.cart-items')
+const cartTotalDOM = getElement('.cart-total')
+
+let cart = getStorageItem('cart')
+
 export const addToCart = (id) => {
   openCart()
 }
+
+const init = () => {}
+
+init()
